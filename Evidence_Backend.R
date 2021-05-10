7f5963287da788a148bca9133729919821344530
Clinton Tedja <clintontedja@gmail.com>
2021-05-10 19:55:33 +0700

    Update Evidence_Backend.R

--- Evidence_Backend.R
+++ Evidence_Backend.R
@@ -12,7 +12,6 @@ library(sf)
 
 
 
-
 # 1. Manual Evidence Database ----
 
 # First we load our manually-input master database of evidence products,
@@ -23,22 +22,14 @@ library(sf)
 evidence <- read_excel('C:\\Users\\clinton.tedja\\World Food Programme\\Regional Bureau Bangkok - Programme\\Programme cycle, Reports & KM\\Knowledge Management\\CSP Evidence Master List.xlsx', sheet = 'Evidence', skip = 3)
 
 
+# First we gotta do a pivot to allow for tidier long format of coverage at subnational
+#   levels which are manually comma separated in the original database
 nmax <- max(str_count(evidence$Coverage, "\\,"))
 
 
 evidence <- separate(data = evidence, col = Coverage, 
                   into = paste0("c", seq_len(nmax)),
                   sep = "\\,")
-
-
-evidence %>% pivot_longer(cols = paste0("c", seq_len(nmax)),
-                          names_to = "Coverage_N",
-                          values_to = "Coverage",
-                          values_drop_na = TRUE) %>%
-  select(-c(Coverage_N, Index, Theme, Source)) %>%
-  mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30")) %>%
-  mutate(Source = "Manual Entry", 
-         Origin_Link = NA)
 
 
 evidence <- evidence %>% pivot_longer(cols = paste0("c", seq_len(nmax)),
@@ -49,6 +40,23 @@ evidence <- evidence %>% pivot_longer(cols = paste0("c", seq_len(nmax)),
   mutate(Date = as.Date(as.numeric(Date), origin = "1899-12-30")) %>%
   mutate(Source = "Manual Entry", Origin_Link = NA, Image = NA) %>%
   mutate(Coverage = str_trim(Coverage, side = c("left"))) 
+
+# Then we gotta repeat that pivot for any places where country titles
+#   were comma separated in the same way
+nmax <- max(str_count(evidence$Country, "\\,"))
+
+evidence <- separate(data = evidence, col = Country, 
+                     into = paste0("c", seq_len(nmax)),
+                     sep = "\\,")
+
+evidence <- evidence %>% pivot_longer(cols = paste0("c", seq_len(nmax)),
+                                      names_to = "Country_N",
+                                      values_to = "Country",
+                                      values_drop_na = TRUE) %>%
+  select(-c(Country_N)) %>%
+  mutate(Country = str_trim(Country))
+
+
 
 
 
@@ -142,9 +150,9 @@ reliefweb_list <- c(content(reliefweb_raw_2021)$data,
 
 
 # Now let's build our dataframe.
-reliefweb_df <- bind_rows(lapply(reliefweb_list, as.data.frame)) %>%
-  select(c(fields.date.created, 
-           fields.country.name,
+reliefweb_df <- bind_rows(lapply(reliefweb_list, as.data.frame))%>%
+  select(c(fields.date.created,
+           fields.primary_country.name,
            fields.format.name,
            fields.source.shortname,
            fields.title,
@@ -157,7 +165,7 @@ reliefweb_df <- bind_rows(lapply(reliefweb_list, as.data.frame)) %>%
                                           format = "%Y-%m-%d", 
                                           tz = "UTC")) %>%
   # Let's rename these to match our manual dataset
-  rename(Country = fields.country.name,
+  rename(Country = fields.primary_country.name,
          Category = fields.format.name,
          Title = fields.title,
          Date = fields.date.created,
@@ -178,15 +186,22 @@ reliefweb_df <- bind_rows(lapply(reliefweb_list, as.data.frame)) %>%
   # And that some certain values are correctly categorized.
   mutate(Category = case_when(grepl("market", Title, ignore.case = TRUE) ~ "Market/Price Monitoring",
                               grepl("price", Title, ignore.case = TRUE) ~ "Market/Price Monitoring",
+                              grepl("annual country report", Title, ignore.case = TRUE) ~ "Annual Country Report",
                               TRUE ~ Category)) %>%
   mutate(Sharing = "Public", 
          Recommendations = NA, 
          Source = "ReliefWeb API") %>%
+  # And that any abberations in country names match our spatial file later
+  mutate(Country = case_when(
+    Country == "Lao People's Democratic Republic (the)" ~ "Lao People's Democratic Republic",
+    Country == "American Samoa" ~ "Samoa",
+    TRUE ~ Country)) %>%
   # And these country briefs appear to have been picked up so 
   #   intermittently by ReliefWeb so we'll leave it for now.
   filter(!grepl("country brief", Title, ignore.case = TRUE)) %>%
   # And we want to also get rid of all the superfluous asterisks, for consistency.
   mutate(Summary = gsub("\\*", "", Summary)) 
+
 
 
 
@@ -207,7 +222,7 @@ reliefweb_df <- reliefweb_df %>%
            !Alt_Link %in% duplicates &
            !Origin_Link %in% duplicates)
 
-
+unique(reliefweb_df$Country)
 
 #
 #
@@ -224,7 +239,9 @@ reliefweb_df <- reliefweb_df %>%
 
 # First let's read in a corporate shapefile which has all subnational boundaries
 #   down to administrative level 2 (we use these to then build our maps later)
-geography <- read_sf("C:\\Users\\clinton.tedja\\OneDrive - World Food Programme\\Documents (OneDrive)\\Data\\RBB_administrative_boundary_polygon_shapefile\\RBB_administrative_boundary_polygon\\RBB_bnd_adm2_WFP.shp")
+geography <- read_sf("C:\\Users\\clinton.tedja\\OneDrive - World Food Programme\\Documents (OneDrive)\\Data\\rbb_adm2_may_2021\\adm2.shp")
+
+
 
 
 # The following function does the work of automating the
@@ -243,12 +260,6 @@ reliefweb_df <- map_df(seq(nrow(reliefweb_df)), function(x) {
   # The function applies a logical T/F vector for all rows in our df;
   #   here it's checking if any admin names exist within our titles or summaries
   matches <- (str_detect(
-    reliefweb_df$Summary[x],
-    (filter(geography, adm0_name == reliefweb_df$Country[x]))$adm1_name) | 
-      str_detect(
-        reliefweb_df$Summary[x], 
-        (filter(geography, adm0_name == reliefweb_df$Country[x]))$adm2_name) |
-      str_detect(
         reliefweb_df$Title[x],
         (filter(geography, adm0_name == reliefweb_df$Country[x]))$adm1_name) | 
       str_detect(
@@ -266,8 +277,9 @@ reliefweb_df <- map_df(seq(nrow(reliefweb_df)), function(x) {
 #   and which don't actually have any subnational level items.
 #   We can manually review using the following:
 #     sort(table((filter(admin_matching, Locations != Country))$Locations), decreasing = TRUE)[1:100]
-  filter(!Coverage %in% c("Ba", "Reg"))
+  filter(!Coverage %in% c("Ba", "Reg")) 
 
+reliefweb_df$Coverage
 
 
 
@@ -281,6 +293,8 @@ reliefweb_df <- map_df(seq(nrow(reliefweb_df)), function(x) {
 # 4. Combined Data ----
 
 # Then let's bind our reliefweb database with the manual database
+all_equal(evidence, reliefweb_df)
+
 combined_data <- rbind(evidence, reliefweb_df) %>%
   # But we also need to consolidate the links so that there's only one, preferably
   # giving priority to Origin_Link which provides us with any of the original
@@ -292,11 +306,13 @@ combined_data <- rbind(evidence, reliefweb_df) %>%
   arrange(Country, Category)
 
 
+#
+#
+#
 
-# NEXT ----
-# Next steps:
-#   1) Ensure correct matching of adm0_names
 
+
+# Tests and Export ----
 test <- anti_join(combined_data, geography, by = c("Country" = "adm0_name"))
 test2 <- anti_join(reliefweb_df, geography, by = c("Country" = "adm0_name"))
 test3 <- anti_join(evidence, geography, by = c("Country" = "adm0_name"))
@@ -307,17 +323,11 @@ unique(test3$Country)
 unique(geography$adm0_name)
 
 
-
 plot_data <- combined_data %>% filter(grepl("WFP", Author)) %>%
   distinct(Title, .keep_all = TRUE)
 
-
-
 ggplot(plot_data, aes(x = Date, fill = Source)) + 
   geom_histogram(binwidth = 180)
-
-
-
 
 
 # Now I'm adding this to the original excel sheet on our Sharepoint
@@ -330,7 +340,10 @@ write.xlsx(as.data.frame(combined_data),
            'C:\\Users\\clinton.tedja\\World Food Programme\\Regional Bureau Bangkok - Programme\\Programme cycle, Reports & KM\\Knowledge Management\\CSP Evidence Master List.xlsx')
 
 
+sample_n(combined_data, 20)
+unique(combined_data$Country)
 
+sample_n(filter(combined_data, Source == "ReliefWeb API"), 20)
 # As well as adding it to a OneDrive location that will allow
 #   for immediate syncing 
 write.xlsx(as.data.frame(combined_data), 
